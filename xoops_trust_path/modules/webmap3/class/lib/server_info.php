0dcd5f88c6401178ff521c5e28b1a2dc1b4c8f28
nao-pon <hypweb@gmail.com>
2014-02-16 18:32:07 +0900

    support XOOPS_DB_TYPE: `mysqli`

--- xoops_trust_path/modules/webmap3/class/lib/server_info.php
+++ xoops_trust_path/modules/webmap3/class/lib/server_info.php
@@ -32,9 +32,11 @@ function webmap3_lib_server_info()
 //---------------------------------------------------------
 function build_server()
 {
+	$db = XoopsDatabaseFactory::getDatabaseConnection();
+	$get_server_info = (is_object($db->conn) && get_class($db->conn) === 'mysqli')? 'mysqli_get_server_info' : 'mysql_get_server_info';
 	$str  = "OS: ". php_uname() ."<br />\n"; 
 	$str .= "PHP: ". PHP_VERSION ."<br />\n"; 
-	$str .= "MySQL: ". mysql_get_server_info() ."<br />\n"; 
+	$str .= "MySQL: ". $get_server_info($db->conn) ."<br />\n"; 
 	$str .= "XOOPS: ". XOOPS_VERSION ."<br />\n"; 
 	return $str;
 }
